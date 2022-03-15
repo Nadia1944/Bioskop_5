dc18aa95f275f77466e520d74f186e59d74c784a
Nadia1944 <63362943+Nadia1944@users.noreply.github.com>
2022-03-15 16:26:17 +0700

    2

--- SI_Bioskop/SI_Bioskop/SI_Bioskop/Crew.Designer.vb
+++ SI_Bioskop/SI_Bioskop/SI_Bioskop/Crew.Designer.vb
@@ -165,11 +165,11 @@ Partial Class Crew
         'Label12
         '
         Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(10, 138)
+        Me.Label12.Location = New System.Drawing.Point(11, 138)
         Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(34, 13)
+        Me.Label12.Size = New System.Drawing.Size(79, 13)
         Me.Label12.TabIndex = 29
-        Me.Label12.Text = "Usia :"
+        Me.Label12.Text = "Usia                :"
         '
         'status
         '
@@ -246,81 +246,81 @@ Partial Class Crew
         Me.Label10.AutoSize = True
         Me.Label10.Location = New System.Drawing.Point(13, 353)
         Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(43, 13)
+        Me.Label10.Size = New System.Drawing.Size(76, 13)
         Me.Label10.TabIndex = 8
-        Me.Label10.Text = "Status :"
+        Me.Label10.Text = "Status            :"
         '
         'Label9
         '
         Me.Label9.AutoSize = True
         Me.Label9.Location = New System.Drawing.Point(10, 250)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(45, 13)
+        Me.Label9.Size = New System.Drawing.Size(78, 13)
         Me.Label9.TabIndex = 7
-        Me.Label9.Text = "Alamat :"
+        Me.Label9.Text = "Alamat            :"
         '
         'Label8
         '
         Me.Label8.AutoSize = True
         Me.Label8.Location = New System.Drawing.Point(10, 220)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(45, 13)
+        Me.Label8.Size = New System.Drawing.Size(78, 13)
         Me.Label8.TabIndex = 6
-        Me.Label8.Text = "No HP :"
+        Me.Label8.Text = "No HP            :"
         '
         'Label7
         '
         Me.Label7.AutoSize = True
         Me.Label7.Location = New System.Drawing.Point(10, 193)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(46, 13)
+        Me.Label7.Size = New System.Drawing.Size(79, 13)
         Me.Label7.TabIndex = 5
-        Me.Label7.Text = "Agama :"
+        Me.Label7.Text = "Agama            :"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(10, 166)
+        Me.Label6.Location = New System.Drawing.Point(9, 166)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(77, 13)
+        Me.Label6.Size = New System.Drawing.Size(80, 13)
         Me.Label6.TabIndex = 4
-        Me.Label6.Text = "Jenis Kelamin :"
+        Me.Label6.Text = "Jenis Kelamin  :"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
         Me.Label5.Location = New System.Drawing.Point(10, 115)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(78, 13)
+        Me.Label5.Size = New System.Drawing.Size(81, 13)
         Me.Label5.TabIndex = 3
-        Me.Label5.Text = "Tanggal Lahir :"
+        Me.Label5.Text = "Tanggal Lahir  :"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Location = New System.Drawing.Point(10, 86)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(75, 13)
+        Me.Label4.Size = New System.Drawing.Size(81, 13)
         Me.Label4.TabIndex = 2
-        Me.Label4.Text = "Tempat Lahir :"
+        Me.Label4.Text = "Tempat Lahir   :"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(10, 59)
+        Me.Label3.Location = New System.Drawing.Point(11, 59)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(68, 13)
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
         Me.Label3.TabIndex = 1
-        Me.Label3.Text = "Nama Crew :"
+        Me.Label3.Text = "Nama Crew     :"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(10, 32)
+        Me.Label2.Location = New System.Drawing.Point(11, 32)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(51, 13)
+        Me.Label2.Size = New System.Drawing.Size(81, 13)
         Me.Label2.TabIndex = 0
-        Me.Label2.Text = "ID Crew :"
+        Me.Label2.Text = "ID Crew           :"
         '
         'foto_diri
         '
