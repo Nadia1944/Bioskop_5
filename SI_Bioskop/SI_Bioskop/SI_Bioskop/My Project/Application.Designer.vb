923ef4a4ebeebd6f321c36d637f792194b9f00a0
Nadia1944 <63362943+Nadia1944@users.noreply.github.com>
2022-03-28 15:50:07 +0700

    coba foto id card

--- SI_Bioskop/SI_Bioskop/SI_Bioskop/My Project/Application.Designer.vb
+++ SI_Bioskop/SI_Bioskop/SI_Bioskop/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SI_Bioskop.QR_Code_reader_Webcam
+            Me.MainForm = Global.SI_Bioskop.Login
         End Sub
     End Class
 End Namespace
